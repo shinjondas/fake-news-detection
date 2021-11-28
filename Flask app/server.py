085677946d17831e03c40d32fd8da8cb37e9e13e
AKSHAT VIDYARTHI <akshat.vid@gmail.com>
2021-11-28 12:33:46 +0530

    new app

---
+++ Flask app/server.py
@@ -0,0 +1,31 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+app = Flask(__name__)
+
+model = pickle.load(open('detector_model.pkl', 'rb'))
+
+vectorizer = pickle.load(open('vectorizer1.pkl','rb'))
+tfvect = TfidfVectorizer(stop_words='english', max_df=0.7)
+
+@app.route('/')
+def form():
+    return render_template('form1.html')
+
+@app.route('/predict', methods = ['POST'])
+def predict():
+    if request.method == 'POST':
+        form_data = request.form
+        # prediction=True
+        # vectorized_input_data = vectorizer.transform(form_data['name'])
+        prediction = model.predict([form_data['name']])
+        
+        print(form_data['name'])
+        return render_template('form1.html', form_data = form_data, prediction=prediction)
+
+        
+        
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000,debug=True)
